0c4fa11cc1ec1dc9e82a42ad886196f9f148fc8b
Albert <albert@cuadrantedigital.com>
2024-02-21 21:34:44 -0500

    Inicial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A314 Studio tools
---
+++ src/ToolsHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace A314\Helper;
+
+class ToolsHelper
+{
+    public function __construct()
+    {
+    }
+
+    public static function test()
+    {
+	    echo "Hola Mundo";
+    }
+}
+
